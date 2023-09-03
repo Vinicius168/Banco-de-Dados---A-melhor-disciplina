714c2c1d0da840186bfc0696580fff80c86683de
Vinicius <viniciusgames168@gmail.com>
2023-09-03 19:32:21 -0300

    exercicio 3

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -2,3 +2,8 @@ select titulo from livros;
 
 select NOME from autores
 WHERE nascimento < '1900-01-01';
+
+SELECT titulo 
+from livros 
+inner join autores on autor_id=autores.id
+where NOME = 'J.K. Rowling';
