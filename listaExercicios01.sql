4bc4ed5744713cb9d50644b662d72107e52b43c5
Vinicius <viniciusgames168@gmail.com>
2023-09-03 21:13:40 -0300

    exercicio 20

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,39 +1,59 @@
+-- exercício 1
+
 select titulo from livros;
+
+-- exercício 2
 
 select NOME from autores
 WHERE nascimento < '1900-01-01';
+
+-- exercício 3
 
 SELECT titulo 
 from livros 
 inner join autores on autor_id=autores.id
 where NOME = 'J.K. Rowling';
 
+-- exercício 4
+
 select alunos.nome
 from alunos 
 join matriculas on alunos.id = matriculas.aluno_id
 where matriculas.curso = 'Engenharia de Software';
 
+-- exercício 5
+
 SELECT produto, SUM(receita) as  receita_total
 FROM vendas
 GROUP BY produto;
+
+-- exercício 6
 
 select autores.nome, count(livros.id) as numero_de_livros
 from autores
 left join livros on autores.id = livros.autor_id
 group by autores.id;
 
+-- exercício 7
+
 select curso, count(aluno_id) as numero_de_alunos
 from matriculas
 group by curso;
+
+-- exercício 8
 
 SELECT produto, AVG(receita) AS media_receita
 FROM vendas
 GROUP BY produto;
 
+-- exercício 9
+
 SELECT produto, SUM(receita) AS receita_total
 FROM vendas
 GROUP BY produto
 HAVING SUM(receita) > 10000;
+
+-- exercício 10
 
 SELECT autores.nome, COUNT(livros.id) AS numero_de_livros
 FROM autores
@@ -41,25 +61,37 @@ LEFT JOIN livros ON autores.id = livros.autor_id
 GROUP BY autores.id
 HAVING COUNT(livros.id) > 2;
 
+-- exercício 11
+
 SELECT livros.titulo, autores.nome AS autor
 FROM livros
 JOIN autores ON livros.autor_id = autores.id;
+
+-- exercício 12
 
 SELECT alunos.nome, matriculas.curso
 FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id;
 
+-- exercício 13
+
 SELECT autores.nome, livros.titulo
 FROM autores
 LEFT JOIN livros ON autores.id = livros.autor_id;
+
+-- exercício 14
 
 SELECT matriculas.curso, alunos.nome
 FROM matriculas
 RIGHT JOIN alunos ON matriculas.aluno_id = alunos.id;
 
+-- exercício 15 
+
 SELECT alunos.nome, matriculas.curso
 FROM alunos
 INNER JOIN matriculas ON alunos.id = matriculas.aluno_id;
+
+-- exercício 16
 
 SELECT autores.nome, COUNT(livros.id) AS numero_de_livros
 FROM autores
@@ -68,9 +100,13 @@ GROUP BY autores.id
 ORDER BY numero_de_livros DESC
 LIMIT 1;
 
+-- exercício 17
+
 SELECT produto, MIN(receita) AS menor_receita
 FROM vendas
 GROUP BY produto;
+
+-- exercício 18
 
 SELECT autores.nome, SUM(livros_vendidos.qtd_vendida * 20) AS receita_total
 FROM autores
@@ -82,7 +118,17 @@ LEFT JOIN (
 ) AS livros_vendidos ON livros.id = livros_vendidos.livro_id
 GROUP BY autores.id;
 
+-- exercício 19
+
 SELECT alunos.nome, COUNT(matriculas.id) AS numero_de_matriculas
 FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
 GROUP BY alunos.id;
+
+-- exercício 20
+
+SELECT produto, COUNT() AS quantidade_de_vendas
+FROM vendas
+GROUP BY produto
+ORDER BY quantidade_de_vendas DESC
+LIMIT 1;
